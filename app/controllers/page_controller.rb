a047fcb96c5fc1cb454c95560a2f013a8c91cd87
troythewar <troythewar24@gmail.com>
2013-10-01 13:03:51 +0530

    Controller-->Views variables usage... Using the parameters in the URL using params

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,13 @@
 class PageController < ApplicationController
   def home
+	  @time=Time.now
+	  @deepak="Hello Rohan"
+	  @url1=params[:name]
+	  	#params are used to fetch the strings passed in the url...
+	  	#In the above case I am passing the name in URL e.g. #?name=rohan
+	  	#Page has to display Rohan when @url is called
+	  	#http://localhost:3000/page/home?name=rohan&year=2013
+	  @url2=params[:year]
   end
 
   def tech
