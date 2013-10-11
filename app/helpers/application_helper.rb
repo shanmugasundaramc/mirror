1686a8fb2da30ddf3589e9079a038815554ab4c0
troythewar <troythewar24@gmail.com>
2013-10-11 22:05:06 +0530

    Updated on menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
 	def calender(month, year)	
 		prev_month=link_to "Previous",page_home_path(:month=>@month.to_i - 1)
+		next_month=link_to "Next",page_home_path(:month=>@month.to_i + 1)
 		calc_str="<table border='1'>"
 		calc_str+="<tr>"
-		calc_str+="<tr><td>#{prev_month}</td><td colspan=6>October</td></tr>"
+		calc_str+="<tr><td>#{prev_month}</td><td colspan=6>October</td><td>#{next_month}</td></tr>"
 		calc_str+="<td>&nbsp;</td>"
 			6.times do |day|
 			calc_str+="<td>#{day +1}</td>"
